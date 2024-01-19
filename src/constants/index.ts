44c2b2f21ad63a07d82ca78aa50b5e2ef2ff822f
austin230196 <austinakamelu55@gmail.com>
2024-01-19 16:10:24 +0100

    Updated code room

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,5 @@
 export const BASE_URL = "http://127.0.0.1:9000/api/v1";
+
+export const JUDGE0_URL = 'https://judge0-ce.p.rapidapi.com';
+export const JUDGE0_KEY = '004d3e1a98mshb607e8b80903155p1635b6jsn5325da0801dc';
+export const JUDGE0_HOST='judge0-ce.p.rapidapi.com';
--- src/pages/CodeRoom/index.tsx
+++ src/pages/CodeRoom/index.tsx
@@ -1,4 +1,4 @@
-import {useRef, useEffect} from "react";
+import {useRef, useMemo, useState} from "react";
 import {Editor, OnChange, useMonaco} from "@monaco-editor/react";
 import * as Y from "yjs";
 import {WebsocketProvider} from "y-websocket";
@@ -7,50 +7,97 @@ import {editor} from "monaco-editor";
 
 
 import useLocalStorage from "../../hooks/useLocalStorage";
+import axiosInstance from "../../axios";
+import { JUDGE0_HOST, JUDGE0_KEY, JUDGE0_URL } from "../../constants";
 
 
 const CodeRoom = () => {
     const monaco = useMonaco();
-    const {value, clear, setItem} = useLocalStorage("theme");
-    const editorRef = useRef<editor.IStandaloneCodeEditor>();
-    const v = useRef(value);
-    const url = "ws://127.0.0.1:9000";
-    const validLanguages = useRef();
+    const {value: va, clear, setItem} = useLocalStorage("theme");
+    const [code, setCode] = useState("");
+    // const editorRef = useRef<editor.IStandaloneCodeEditor>();
+    const [defaultLanguage, setDefaultLanguage] = useState("rust");
+    const v = useRef(va);
+    // const url = "ws://127.0.0.1:9000";
+    const validLanguages = monaco?.languages.getLanguages().map(l => l.id);
+    console.log({validLanguages, l: monaco?.languages.getLanguages()})
 
-    useEffect(() => {}, [value]);
-    console.log(monaco?.languages.getLanguages());
+    const compilerLanguages = useMemo(async () => {
+        const res = await axiosInstance.get(`${JUDGE0_URL}/languages`, {
+            headers: {
+                'X-RapidAPI-Key': JUDGE0_KEY,
+                'X-RapidAPI-Host': JUDGE0_HOST,
+                'Content-Type': 'application/json',
+                'content-type': 'application/json'
+            }
+        })
+        return res.data
+    }, []);
     
     function handleEditorDidMount(editor: editor.IStandaloneCodeEditor) {
-        editorRef.current = editor;
+        // editorRef.current = editor;
+        console.log({editor});
 
-        //initialize yjs
-        const doc = new Y.Doc();
+        // //initialize yjs
+        // const doc = new Y.Doc();
 
-        //connect to peers with websocket
-        const provider: WebsocketProvider = new WebsocketProvider(url, "roomId", doc);
-        const type = doc.getText("monaco");
+        // //connect to peers with websocket
+        // const provider: WebsocketProvider = new WebsocketProvider(url, "roomId", doc);
+        // const type = doc.getText("monaco");
 
-        //bind yjs binding to monaco binding
-        const binding = new MonacoBinding(type, editorRef.current.getModel()!, new Set([editorRef.current]), provider.awareness);
+        // //bind yjs binding to monaco binding
+        // const binding = new MonacoBinding(type, editorRef.current.getModel()!, new Set([editorRef.current]), provider.awareness);
 
-        console.log({binding, provider, theme: value});
+        // console.log({binding, provider, theme: value});
     }
 
     function handleEditorChange(value: string, event: editor.IModelContentChangedEvent){
-        console.log({event, value});
+        console.log({event});
+        setCode(() => value);
+        handleCompile()
     }
 
-    console.log(v);
+    async function handleCompile(){
+        try{
+            const formdata = {
+                // language_id: ,
+                source_code: btoa(code),
+            }
+            const res = await axiosInstance.post(`${JUDGE0_URL}/submissions`, formdata, {
+                params: {
+                    base64_encoded: true,
+                    fields: '*'
+                },
+                headers: {
+                    'X-RapidAPI-Key': JUDGE0_KEY,
+                    'X-RapidAPI-Host': JUDGE0_HOST,
+                    'Content-Type': 'application/json',
+                    'content-type': 'application/json'
+                }
+            })
+        }catch(e: any){
+            console.log(e.message);
+        }
+    }
+
+    console.log(compilerLanguages);
 
 
     return (
         <div className="">
+            <select value={defaultLanguage} onChange={e => setDefaultLanguage(() => e.target.value)}>
+                {
+                    validLanguages?.map((l: string, i: number) => (
+                        <option value={l} key={i}>{l.toLocaleLowerCase()}</option>
+                    ))
+                }
+            </select>
             <Editor
             height={"100vh"}
-            language="rust"
-            defaultLanguage="cpp"
-            defaultValue={""}
-            theme={value === 'dark' ? "vs-dark" : "vs-light"}
+            width={"100%"}
+            value={code}
+            language={defaultLanguage}
+            theme={v.current === 'dark' ? "vs-dark" : "vs-light"}
             onChange={handleEditorChange as OnChange}
             onMount={handleEditorDidMount}
             />
